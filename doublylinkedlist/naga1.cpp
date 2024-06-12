7eeac72a0f2d05901735f30bb5bd52e612e43b0f
nagasai1155 <nagasaibalam@gmail.com>
2024-06-12 11:41:18 +0530

    pracicedoubleylinkedlistagain

--- doublylinkedlist/naga1.cpp
+++ doublylinkedlist/naga1.cpp
@@ -44,11 +44,36 @@ int  searchaneleindll(Node* head,int value){
        temp = temp->next;
 return 0;
 }
+
+Node* deletebeginning(Node* head){
+    if(head == NULL) return NULL;
+    Node* temp = head;
+    head = head->next;
+    
+    head->back = nullptr;
+    temp->next = nullptr;
+   free(temp);
+    return head;
+}
+
+Node* deleteatend(Node* head){
+    if(head== NULL) return NULL;
+    Node* temp = head;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    Node* prev = temp->back;
+    prev->next = nullptr;
+    temp->back= nullptr;
+    free(temp);
+    return head;
+}
 int main(){
     vector<int> arr = {3,5,2,7,8};
-    Node* head = arrtodll(arr);
-    int val = searchaneleindll(head,3);
-    cout<<val<<endl;
+    Node* head1 = arrtodll(arr);
+    // int val = searchaneleindll(head,3);
+    // cout<<val<<endl;
+    Node* head = deleteatend(head1);
     Node* temp = head;
     while(temp != NULL){
         cout<<temp->data<<" ";
