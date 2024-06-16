0e023e8b32a6552b24f0b2f3c29c7299e73915ab
nagasai1155 <nagasaibalam@gmail.com>
2024-06-16 21:25:46 +0530

    subsequences

---
+++ recursionfordppractice/printsubsequences.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//printing all subsequences from given array or vector<int> arr;
+void fun(int index ,int n,int arr[],vector<int> &ds){
+    if(index == n){
+        for(auto it: ds){
+            cout<<it<<" ";
+        }
+        if(ds.size()==0){
+            cout<<"[]";
+        }
+        cout<<endl;
+        return;
+    }
+    ds.push_back(arr[index]);
+    fun(index+1,n,arr,ds);
+    ds.pop_back();
+    fun(index+1,n,arr,ds);
+    return;
+
+}
+int main(){
+    int n=3;
+    int arr[]={3,1,2};
+    vector<int> ds;
+    fun(0,n,arr,ds);
+}
