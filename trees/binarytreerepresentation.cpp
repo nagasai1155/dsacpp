4eaa8c6813f53cf7b0ff8526bdef0a57f82b2390
nagasai1155 <nagasaibalam@gmail.com>
2024-06-19 09:49:34 +0530

    binray tree

---
+++ trees/binarytreerepresentation.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+     
+     public: 
+     Node(int data1){
+data = data1;
+left= right = NULL;
+     }
+
+};
+void preorder(Node* root){
+    if(root == NULL) return;
+    else{
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void inorder(Node* root){
+    if(root == NULL) return;
+    else{
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+void postorder(Node* root){
+    if(root == NULL) return;
+    else{
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" ";
+    }
+}
+int main(){
+
+Node* root = new Node(1);
+root->left= new Node(2);
+root->right = new Node(6);
+root->left->left = new Node(4);
+root->right->left= new Node(9);
+root->right->right = new Node(44);
+cout<<"preorder: ";
+preorder(root);
+cout<<endl;
+cout<<"inorder:";
+inorder(root);
+cout<<endl;
+cout<<"postorder :";
+postorder(root);
+
+
+}
