507ebfdb15843fe681f8eb65f2da4af37eb7d04a
nagasai1155 <nagasaibalam@gmail.com>
2024-06-28 11:47:47 +0530

    string day 2 problems

--- strings/day2 problems/replace_substr_instring.cpp
+++ strings/day2 problems/replace_substr_instring.cpp
@@ -1,12 +1,16 @@
 //replace the substring in the given string in c++
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main(){
+// int main(){
 
-    string str = "hello world";
-    str.replace(6,8,"kumar");
-    cout<<str<<endl;
-}
+//     string str = "hello world";
+//     str.replace(6,8,"kumar");
+//     cout<<str<<endl;
+// }
+
+
+
+//replace the word another word from the given string in c++
