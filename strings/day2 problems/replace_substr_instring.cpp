9cf50111770534cb54d48b8969271b3063163677
nagasai1155 <nagasaibalam@gmail.com>
2024-06-28 11:23:48 +0530

    string problems

---
+++ strings/day2 problems/replace_substr_instring.cpp
@@ -0,0 +1,12 @@
+//replace the substring in the given string in c++
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str = "hello world";
+    str.replace(6,8,"kumar");
+    cout<<str<<endl;
+}
